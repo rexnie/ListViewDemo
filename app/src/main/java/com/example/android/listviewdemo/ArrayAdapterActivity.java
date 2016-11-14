0bd127248036e2384ddf8d53e1b3681a567c7261
niedaocai <niedaocai@snda.com>
2016-11-15 00:24:20 +0800

    ListView使用ArrayAdapter+simple_list_item_1填充数据

---
+++ app/src/main/java/com/example/android/listviewdemo/ArrayAdapterActivity.java
@@ -0,0 +1,22 @@
+package com.example.android.listviewdemo;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class ArrayAdapterActivity extends AppCompatActivity {
+    private ListView mListView;
+    private String[] mNumbers;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_array_adapter);
+        mNumbers = getResources().getStringArray(R.array.numbers);
+
+        mListView = (ListView) findViewById(R.id.lv_number);
+        mListView.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,
+                mNumbers));
+    }
+}
