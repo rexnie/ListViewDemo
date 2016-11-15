e00d8fda61c9e2cc58f65338411ef66a7e13d3a8
niedaocai <niedaocai@snda.com>
2016-11-15 10:12:03 +0800

    给ListView添加item点击事件

--- app/src/main/java/com/example/android/listviewdemo/ArrayAdapterActivity.java
+++ app/src/main/java/com/example/android/listviewdemo/ArrayAdapterActivity.java
@@ -2,10 +2,14 @@ package com.example.android.listviewdemo;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 public class ArrayAdapterActivity extends AppCompatActivity {
+    private static final String TAG = "ArrayAdapterActivity";
     private ListView mListView;
     private String[] mNumbers;
 
@@ -20,5 +24,16 @@ public class ArrayAdapterActivity extends AppCompatActivity {
                 android.R.layout.simple_list_item_single_choice,
                 mNumbers));
         mListView.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
+
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                /* i: View 在AdapterView中的位置
+                *  l: View绑定的Item在data source中的位置
+                *  此处是Array, 所以两者有相同的值*/
+                Log.d(TAG, "onItemClick: i=" + i + ",l=" + l);
+                setTitle("你点击了第" + i + "行");
+            }
+        });
     }
 }
