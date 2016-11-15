46f9175eb920f64a67c5a905ed7e4f160247259d
niedaocai <niedaocai@snda.com>
2016-11-15 09:29:40 +0800

    layout 使用simple_list_item_multiple_choice simple_list_item_checked: 带选择框的ListView simple_list_item_multiple_choice: 带CheckBox的ListView

--- app/src/main/java/com/example/android/listviewdemo/ArrayAdapterActivity.java
+++ app/src/main/java/com/example/android/listviewdemo/ArrayAdapterActivity.java
@@ -17,7 +17,7 @@ public class ArrayAdapterActivity extends AppCompatActivity {
 
         mListView = (ListView) findViewById(R.id.lv_number);
         mListView.setAdapter(new ArrayAdapter<>(this,
-                android.R.layout.simple_list_item_checked,
+                android.R.layout.simple_list_item_multiple_choice,
                 mNumbers));
         mListView.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
     }
